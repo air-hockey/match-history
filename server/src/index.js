9d0bd04a52e55801837fa2d36b37c6a5df7f4e67
Mark Brouch <mark.brouch@gmail.com>
2018-05-16 22:50:44 -0500

    🎉 Create graphql server

---
+++ server/src/index.js
@@ -0,0 +1,54 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
+
+const resolvers = {
+  Query: {
+    matches: (_, { winner }, context, info) => context.prisma.query.matches({
+      where: {
+        winner: {
+          name: winner
+        }
+      }
+    }, info),
+    player: (_, { id }, context, info) => context.prisma.query.player({
+      where: { id },
+		}, info),
+		players: (_, args, context, info) => context.prisma.query.players({}, info)
+  },
+  Mutation: {
+    createMatch: (_, { winnerId, loserId }, context, info) => context.prisma.mutation.createMatch({
+			data: {
+				date: new Date(),
+				winner: {
+					connect: {
+						id: winnerId
+					}
+				},
+				loser: {
+					connect: {
+						id: loserId
+					}
+				}
+			}
+		}, info),
+    deleteMatch: (_, { id }, context, info) => context.prisma.mutation.deleteMatch({
+			where: { id }
+		}, info),
+    createPlayer: (_, { name }, context, info) => context.prisma.mutation.createPlayer({
+			data: { name }
+		}, info)
+  }
+}
+
+const server = new GraphQLServer({
+    typeDefs: 'server/src/schema.graphql',
+    resolvers,
+    context: req => ({
+      ...req,
+      prisma: new Prisma({
+        typeDefs: 'server/src/generated/prisma.graphql',
+        endpoint: 'http://localhost:4466',
+      }),
+    }),
+})
+server.start(() => console.log(`GraphQL server is running on http://localhost:4000`))
