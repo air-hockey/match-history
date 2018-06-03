e9ea93f8f1ebb2c3c56a5e9dff0442637a856a92
Mark Brouch <mark.brouch@gmail.com>
2018-06-02 23:41:48 -0500

    ✨ Combine next and yoga servers

---
+++ index.js
@@ -0,0 +1,41 @@
+const next = require('next')
+const server = require('./server/src')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  server.express.get('*', (req, res, next) => {
+    if (req.path.match(/^\/playground|graphql|subscriptions\/?.*/)) {
+      next()
+    } else {
+      return handle(req, res)
+    }
+  })
+
+  server.start(
+    {
+      port,
+      endpoint: '/graphql',
+      subscriptions: '/subscriptions',
+      playground: dev && '/playground'
+    },
+    ({ endpoint, subscriptions, playground }) => {
+      endpoint &&
+        console.log(
+          `> GraphQL server is running on http://localhost:${port}${endpoint}`
+        )
+      subscriptions &&
+        console.log(
+          `> GraphQL subscriptions is running on http://localhost:${port}${subscriptions}`
+        )
+      playground &&
+        console.log(
+          `> GraphQL playground is running on http://localhost:${port}${playground}`
+        )
+      console.log(`> Ready on http://localhost:${port}`)
+    }
+  )
+})
