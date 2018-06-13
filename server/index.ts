fd6eb601d2522647c4f8164f3054ac5dfbcfceca
Mark Brouch <mark.brouch@gmail.com>
2018-06-13 00:11:36 -0500

    🔧 Make routes dynamic

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,16 @@ import * as compression from 'compression'
 
 import { server } from './src'
 
-const dev = process.env.NODE_ENV !== 'production'
+const {
+  NODE_ENV,
+  PORT,
+  SERVER_ENDPOINT,
+  GRAPHQL_ENDPOINT,
+  SUBSCRIPTIONS_ENDPOINT,
+  PLAYGROUND_ENDPOINT
+} = process.env
+
+const dev = NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -11,7 +20,15 @@ app.prepare().then(() => {
   server.express.use(compression())
 
   server.express.get('*', (req, res, next) => {
-    if (req.path.match(/^\/(playground|graphql|subscriptions)((?!.)|\/).*/)) {
+    if (
+      req.path.match(
+        new RegExp(
+          `^(${[PLAYGROUND_ENDPOINT, GRAPHQL_ENDPOINT, SUBSCRIPTIONS_ENDPOINT]
+            .filter(endpoint => endpoint)
+            .join('|')})((?!.)|\/).*`
+        )
+      )
+    ) {
       next()
     } else {
       return handle(req, res)
@@ -20,31 +37,25 @@ app.prepare().then(() => {
 
   server.start(
     {
-      port: parseInt(process.env.PORT, 10) || 3000,
-      endpoint: process.env.GRAPHQL_ENDPOINT,
-      subscriptions: process.env.SUBSCRIPTIONS_ENDPOINT,
-      playground: process.env.PLAYGROUND_ENDPOINT
+      port: parseInt(PORT, 10) || 3000,
+      endpoint: GRAPHQL_ENDPOINT,
+      subscriptions: SUBSCRIPTIONS_ENDPOINT,
+      playground: PLAYGROUND_ENDPOINT
     },
     ({ endpoint, subscriptions, playground, port }) => {
       endpoint &&
         console.log(
-          `> GraphQL server is running on ${
-            process.env.SERVER_ENDPOINT
-          }:${port}${endpoint}`
+          `> GraphQL server is running on ${SERVER_ENDPOINT}:${port}${endpoint}`
         )
       subscriptions &&
         console.log(
-          `> GraphQL subscriptions is running on ${
-            process.env.SERVER_ENDPOINT
-          }:${port}${subscriptions}`
+          `> GraphQL subscriptions is running on ${SERVER_ENDPOINT}:${port}${subscriptions}`
         )
       playground &&
         console.log(
-          `> GraphQL playground is running on ${
-            process.env.SERVER_ENDPOINT
-          }:${port}${playground}`
+          `> GraphQL playground is running on ${SERVER_ENDPOINT}:${port}${playground}`
         )
-      console.log(`> Ready on ${process.env.SERVER_ENDPOINT}:${port}`)
+      console.log(`> Ready on ${SERVER_ENDPOINT}:${port}`)
     }
   )
 })
