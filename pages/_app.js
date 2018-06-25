387931a301b7f356b14669db5a17bd071ea66cd6
Mark Brouch <mark.brouch@gmail.com>
2018-06-24 22:34:48 -0500

    🐛 Fix merge conflict

--- pages/_app.js
+++ pages/_app.js
@@ -2,26 +2,20 @@ import App, { Container } from 'next/app'
 import React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import Head from 'next/head'
-<<<<<<< refs/remotes/origin/master
 import AuthProvider from '../lib/auth-provider'
 import withApolloClient from '../lib/with-apollo-client'
-=======
->>>>>>> WiP reworking PlayerList with semantic-ui
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, apolloClient } = this.props
-    return <Container>
+    return (
+      <Container>
         <Head>
           <title>United States Airhockey Association</title>
-<<<<<<< refs/remotes/origin/master
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.11.8/semantic.min.css"
           />
-=======
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.11.8/semantic.min.css" />
->>>>>>> WiP reworking PlayerList with semantic-ui
         </Head>
         <ApolloProvider client={apolloClient}>
           <AuthProvider>
@@ -29,6 +23,7 @@ class MyApp extends App {
           </AuthProvider>
         </ApolloProvider>
       </Container>
+    )
   }
 }
 
